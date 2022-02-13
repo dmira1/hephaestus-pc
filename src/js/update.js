3a4f2cf23dcf4b0f2c415f51ac7088eeeb919f91
dmira1 <darryl.pmira@gmail.com>
2022-02-12 22:23:14 -0700

    update.js update

--- src/js/update.js
+++ src/js/update.js
@@ -1,12 +1,32 @@
+import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import {ref as databaseRef, set, get} from 'firebase/database';
+import {db, storage} from "./libs/firebase/firebaseConfig";
+
+const productForm = document.forms['product-form']
+
 function pageInit(){
   const key = sessionStorage.getItem('key')
-  console.log("Update Page")
-  console.log(key)
-  // key
-  // get data from RTD
-  // propulate the form with the values
-  // Save the edits ... write text 
+  const productRef = databaseRef(db, `products/${key}`)
+  const productSnapShot = await get(productRef)
+
+  if(productSnapShot.exists()){
+    setFieldValues(productSnapShot.val())
+  }
+
+  productForm.addEventListener('submit', onUpdateRental)
 }
 
+function onUpdateRental(e){
+  e.preventDefault();
+  const city = e.target.elements['product'].value.trim()
+}
+
+function setFieldValues({name, type, price, desc, urlPath}){
+    productForm.elements['#productName'].value = name
+    productForm.elements['#productType'].value = type
+    productForm.elements['#productPrice'].value = price
+    productForm.elements['#productDesc'].desc = desc
+    document.querySelector('.display img').src = urlPath
+}
 
 pageInit();
